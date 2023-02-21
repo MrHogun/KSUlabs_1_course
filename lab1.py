01f37d7eee2b0abde00845a24fc427499879270c
MrHogun <73719169+MrHogun@users.noreply.github.com>
2023-02-21 13:23:35 +0200

    Add files via upload

--- lab1.py
+++ lab1.py
@@ -9,8 +9,8 @@ def subtract_numbers(num1, num2):
 num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
 
-sum = add_numbers(num1, num2)
+summa = add_numbers(num1, num2)
 diff = subtract_numbers(num1, num2)
 
-print("Сумма чисел:", sum)
+print("Сумма чисел:", summa)
 print("Разность чисел:", diff)
